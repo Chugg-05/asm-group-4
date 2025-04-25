97f539a708a4e5a7da44ba5cea59283226dbb6f5
Nguyen Chung <chungnvph52424@gmail.com>
2025-04-25 12:08:13 +0700

    fix Æpath

--- src/main/java/com/example/asm_group_4/controller/AssetController.java
+++ src/main/java/com/example/asm_group_4/controller/AssetController.java
@@ -1,6 +1,7 @@
 package com.example.asm_group_4.controller;
 
 import com.example.asm_group_4.model.Asset;
+import com.example.asm_group_4.model.Staff;
 import com.example.asm_group_4.repository.AssetRepository;
 import jakarta.validation.Valid;
 import lombok.AccessLevel;
@@ -9,6 +10,7 @@ import lombok.experimental.FieldDefaults;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -23,8 +25,7 @@ public class AssetController {
 
     @GetMapping("/list")
     public String getListAsset (Model model, @RequestParam(defaultValue = "0") int page) {
-        int pageSize = 5;
-        Pageable pageable = PageRequest.of(page, pageSize);
+        Pageable pageable = PageRequest.of(page, 5, Sort.by(Sort.Direction.DESC, "id"));
         Page<Asset> pageAsset = assetRepository.findAll(pageable);
 
         model.addAttribute("listA", pageAsset.getContent());
@@ -82,6 +83,6 @@ public class AssetController {
     public String search(@RequestParam("keyword") String keyword, Model model) {
         model.addAttribute("listA", assetRepository.search(keyword));
         model.addAttribute("keyword", keyword);
-        return "/pages/asset";
+        return "pages/asset-list";
     }
 }
